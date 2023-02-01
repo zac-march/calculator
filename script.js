9e23d009836b6518e8edf8326d34ba50ecbfab84
Zachariah March <121649297+zac-march@users.noreply.github.com>
2023-02-01 21:20:42 +1100

    Create functions to handle calculations

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello world")
+
+
+function add(a, b){ return a + b;};
+function subtract (a, b){ return a - b;};
+function multiply (a, b){ return a * b;};
+function divide (a, b){ return a / b;};
+
+function operate(operator, a, b){
+    switch(operator){
+        case '+':
+           return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+    }
+};
+
+console.log(operate('/', 100, 5))
